3a3cee333a90b3ab4476df6c31dddbf9d86253e5
mayur <mayurpurushvani@gmai.com>
2026-01-31 18:09:56 +0530

    Google chrome install code added in yml file

--- cymax_scraper/fetcher.py
+++ cymax_scraper/fetcher.py
@@ -33,6 +33,7 @@ class Fetcher:
         options.add_argument("--start-maximized")
         options.add_argument("--disable-blink-features=AutomationControlled")
         options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-extensions")
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
